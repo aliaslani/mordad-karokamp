49a6136abee630681eb93d4a69d014b7d7c83be0
aliaslani <a.aslani1986@gmail.com>
2025-11-12 20:09:50 +0330

    session2: add User model

---
+++ core/models.py
@@ -0,0 +1,18 @@
+from pyexpat import model
+from turtle import mode
+from django.db import models
+
+
+class CityChoices(models.TextChoices):
+    TEHRAN = ("tehran", "تهران")
+    ISFAHAN = ("isfahan", "اصهان")
+
+
+class User(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=20)
+    birthdate = models.DateField(null=True)
+    bio = models.TextField(null=True)
+    city = models.CharField(
+        max_length=20, choices=CityChoices.choices, default=CityChoices.ISFAHAN
+    )
